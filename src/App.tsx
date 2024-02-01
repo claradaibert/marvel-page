0cd77d3f63f509a871fab758d06993574eba5bbb
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-02-01 09:38:11 -0300

    Add themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+// Theme import
+import { lightTheme } from "./styles/themes/light";
+import { darkTheme } from "./styles/themes/dark";
+import { GlobalStyle } from "./styles/global";
+
+// Page import
+import { Auth } from "./pages/Auth";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <Auth />
+    </ThemeProvider>
   );
 }
 
