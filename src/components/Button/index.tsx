6bb7f5866b219d5652c37ecab5d45a3ec8d30f9d
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-02-02 11:02:13 -0300

    Fix button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { CircularProgress } from "@mui/material";
 
 import { Container } from "./styles";
 
 interface IProps {
   handleClick: () => void;
   text: string;
+  loading?: boolean;
 }
 
-const Button: React.FC<IProps> = ({ handleClick, text }) => {
-  return <Container onClick={() => handleClick()}>{text}</Container>;
+const Button: React.FC<IProps> = ({ handleClick, text, loading }) => {
+  return <Container onClick={() => handleClick()}>{loading ? <CircularProgress /> : text}</Container>;
 };
 
 export { Button };
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -6,8 +6,12 @@ export const Container = styled.button`
     align-items: center;
     justify-content: center;
     border: none;
-    border-radius: 15px;
+    border-radius: 5px;
     background-color: ${({theme}) => theme.contrast_color};
     color: ${({theme}) => theme.pallete_white};
     padding: 1rem;
+
+    span.MuiCircularProgress-root {
+        color: ${({theme}) => theme.pallete_white};
+    }
 `
