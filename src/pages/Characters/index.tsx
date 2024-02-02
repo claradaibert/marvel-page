7f68b966f774f126a69d29dbe6543560653caa0f
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-02-02 12:17:42 -0300

    Add new component to characters

--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
+import { useDebounce } from "use-lodash-debounce";
 
 // Hook import
-import { useAppSelector, useAppDispatch } from "../../hooks/reduxHooks";
-
-// Store action import
-import { setKeys } from "../../store/user/slice";
+import { useAppSelector } from "../../hooks/reduxHooks";
 
 // Component import
-import { Header } from "../../components/Header";
-import { Button } from "../../components/Button";
-import { ListItem } from "../../components/ListItem";
+import { PageLayout } from "../../components/PageLayout";
 
 // Service import
 import { api } from "../../services";
@@ -20,31 +14,29 @@ import { api } from "../../services";
 // Util import
 import { ICharactersResponse } from "../../utils/ResponseModels";
 
-// Inner component import
-import { SearchBar } from "./SearchBar";
-
-import { Container } from "./styles";
-
 const Characters: React.FC = () => {
   // Hooks
   const user = useAppSelector((state) => state.user);
-  const [cookies] = useCookies();
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
 
   // Local states
   const [pageList, setPageList] = useState<ICharactersResponse[]>();
   const [totalItems, setTotalItems] = useState<number>(0);
+  const [searchValue, setSearchValue] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
+  const [searchLoading, setSearchLoading] = useState<boolean>(false);
 
+  const debouncedSearchValue = useDebounce(searchValue, 1000);
   const myPublicKey = user?.publicKey;
 
   const handleSeeMoreClick = async () => {
     setLoading(true);
     try {
       const requestOffset = pageList?.length;
+      const requestSearchValue = searchValue
+        ? `nameStartsWith=${searchValue}&&`
+        : "";
       const response = await api.get(
-        `/characters?offset=${requestOffset}&apikey=${myPublicKey}`
+        `/characters?${requestSearchValue}offset=${requestOffset}&apikey=${myPublicKey}`
       );
       setPageList((prevList) => prevList?.concat(response.data.data.results));
     } catch (err: any) {
@@ -53,17 +45,24 @@ const Characters: React.FC = () => {
     setLoading(false);
   };
 
+  // Add useEffect timer
   useEffect(() => {
-    if (!cookies?.userKeys?.publicKey) {
-      navigate("/");
-    }
+    const handleSearch = async (e: string) => {
+      try {
+        const response = await api.get(
+          `/characters?nameStartsWith=${e}&apikey=${myPublicKey}`
+        );
+        setPageList(response.data.data.results);
+        setTotalItems(response.data.data.total);
+      } catch (err: any) {
+        toast.error(err?.response?.data?.message);
+      }
+    };
 
-    const usersKeys = cookies?.userKeys;
-
-    if (!user?.publicKey) {
-      dispatch(setKeys(usersKeys));
+    if (debouncedSearchValue) {
+      handleSearch(debouncedSearchValue);
     }
-  }, [cookies?.userKeys, user?.publicKey, dispatch, navigate]);
+  }, [debouncedSearchValue, myPublicKey]);
 
   useEffect(() => {
     const getList = async () => {
@@ -75,29 +74,21 @@ const Characters: React.FC = () => {
         toast.error(err?.response?.data?.message);
       }
     };
-    if (user?.publicKey) {
+    if (myPublicKey) {
       getList();
     }
-  }, [user?.publicKey]);
+  }, [myPublicKey]);
 
   return (
-    <Container>
-      <Header />
-      <div className="bodyContainer">
-        <div className="pageTitle">PERSONAGENS</div>
-        <SearchBar setPageList={setPageList} />
-        {pageList?.map((item) => (
-          <ListItem item={item} />
-        ))}
-        {totalItems > (pageList?.length || 0) && (
-          <Button
-            handleClick={() => handleSeeMoreClick()}
-            text="VER MAIS"
-            loading={loading}
-          />
-        )}
-      </div>
-    </Container>
+    <PageLayout
+      pageType="PERSONAGENS"
+      pageList={pageList}
+      handleSeeMoreClick={handleSeeMoreClick}
+      totalItems={totalItems}
+      loading={loading}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+    />
   );
 };
 
