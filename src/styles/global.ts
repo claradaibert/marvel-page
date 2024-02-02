a2d71b03d01ca889b471fd279bdcae84c8004d97
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-02-02 12:37:56 -0300

    Add new images

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const GlobalStyle = createGlobalStyle`
     * {
@@ -8,18 +8,18 @@ const GlobalStyle = createGlobalStyle`
         outline: 0;
         box-sizing: border-box;
         max-width: 100vw;
-        font-family: 'Montserrat', sans-serif;
+        font-family:'Source Sans 3', sans-serif;
 
         ::-webkit-scrollbar {
             width: 0.2rem;
         }
 
         ::-webkit-scrollbar-track {
-            background-color: ${({theme}) => theme.background_primary};
+            background-color: ${({ theme }) => theme.background_primary};
         }
 
         ::-webkit-scrollbar-thumb {
-            background-color: ${({theme}) => theme.contrast_color};
+            background-color: ${({ theme }) => theme.contrast_color};
             border-radius: 10px;
         }
     }
