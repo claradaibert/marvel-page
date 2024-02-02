26e097193f6fa480e1da2edd4aae07214a851940
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-02-02 12:57:10 -0300

    Create new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 
 // import pages
 import { Auth } from "../pages/Auth";
+import { Comics } from "../pages/Comics";
+import { Creators } from "../pages/Creators";
 import { Characters } from "../pages/Characters";
 
 // Create routes
@@ -13,7 +15,15 @@ const router = createBrowserRouter([
   {
     path: '/characters',
     element: <Characters />,
-  }
+  },
+  {
+    path: '/comics',
+    element: <Comics />,
+  },
+  {
+    path: '/creators',
+    element: <Creators />,
+  },
 ]);
 
 export { router };
